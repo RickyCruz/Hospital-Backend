68f4bc8b772456bbc888fe03eaa4db72ec0228e0
Ricky Cruz <rczdev@gmail.com>
2020-04-14 16:15:53 -0500

    Configure nodemon

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/', (request, response, next) => {
+    response.status(200).json({
+        success: true,
+        message: 'Request made correctly'
+    });
+});
+
 app.listen(port, () => {
     console.log(`App listening at port:${port} >\x1b[32m online \x1b[0m`);
 });
