52709f333e697edf95d2403ddf9cb446cba2b2b6
Ricky Cruz <rczdev@gmail.com>
2020-04-14 16:57:45 -0500

    Establish connection between Node and Mongo

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const port = 3000;
+
+mongoose.connect(`mongodb://localhost:27017/mydb`, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+    .then(() => console.log('DB >\x1b[32m conected! \x1b[0m'))
+    .catch(error => console.log('Error', error));
 
 app.get('/', (request, response, next) => {
     response.status(200).json({
