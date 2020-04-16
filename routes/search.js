b782bfccb015664cfee204119ad7b154c939bcc9
Ricky Cruz <rczdev@gmail.com>
2020-04-16 16:32:41 -0500

    Search by specific collection

--- routes/search.js
+++ routes/search.js
@@ -5,6 +5,40 @@ const User = require('../models/user');
 // const authMiddleware = require('../middlewares/authentication');
 const app = express();
 
+// SEARCH PER COLLECTION
+app.get('/collections/:type/:keyword', (request, response) => {
+    let search = request.params.keyword;
+    let collection = request.params.type;
+    let regex = new RegExp(search, 'i');
+    let promise;
+
+    switch(collection) {
+        case 'users':
+            promise = searchUsers(search, regex);
+        break;
+        case 'doctors':
+            promise = searchDoctors(search, regex);
+        break;
+        case 'hospitals':
+            promise = searchHospitals(search, regex);
+        break;
+        default:
+            return response.status(400).json({
+                success: false,
+                message: 'You can olny search: users, hospitals and doctors',
+                error: { message: 'Unknown collection' }
+            });
+    }
+
+    promise.then(data => {
+        return response.status(200).json({
+            success: true,
+            [collection]: data
+        });
+    });
+});
+
+// GLOBAL SEARCH
 app.get('/all/:keyword', (request, response, next) => {
     let search = request.params.keyword;
     let regex = new RegExp(search, 'i');
